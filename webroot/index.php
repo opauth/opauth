3b0c4fa4328e7cb71db6d848ae27eb167baf0f7a
U-Zyn Chua <chua@uzyn.com>
2012-03-08 22:00:08 +0800

    Updated webroot index.php handler to load config

--- webroot/index.php
+++ webroot/index.php
@@ -1,10 +1,24 @@
 <?php
-define('LIB', '../lib/Opauth/');
+/**
+ * Opauth webroot catchall
+ * - Only required if you are sending traffic directly to Opauth's webroot
+ * - If you instantiate Opauth directly from your app or PHP framework, this will not be run.
+ * 
+ */
+define('OPAUTH_ROOT', dirname(dirname(__FILE__)).'/');
+define('OPAUTH_LIB', OPAUTH_ROOT.'lib/Opauth/');
 
-require LIB.'Opauth.php';
-$Opauth = new Opauth( array(
-	'debug' => true,
-	'strategies' => array(
-		'facebook'
-	)
-));
+/**
+ * Load config
+ */
+if (!file_exists(OPAUTH_ROOT.'opauth.conf.php')){
+	trigger_error('Config file missing at '.OPAUTH_ROOT.'opauth.conf.php', E_USER_ERROR);
+	exit();
+}
+require OPAUTH_ROOT.'opauth.conf.php';
+
+/**
+ * Instantiate Opauth with the loaded config
+ */
+require OPAUTH_LIB.'opauth.php';
+$Opauth = new Opauth( $config );

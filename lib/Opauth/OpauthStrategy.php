eefd32b237583cacf7d4ce27fbc16f5cd6d014cd
U-Zyn Chua <chua@uzyn.com>
2012-03-08 22:53:24 +0800

    set compulsory/optional values

--- lib/Opauth/OpauthStrategy.php
+++ lib/Opauth/OpauthStrategy.php
@@ -4,18 +4,68 @@
  * - Individual strategies to be extended from this class
  *
  */
-class OpauthStrategy{
+class OpauthStrategy{	
+/**
+ * Compulsory config keys
+ */
+	public $expects = array('app_id', 'app_secret');
+	
+/**
+ * Optional config keys with respective default values
+ */
+	public $defaults = array(
+		'scope' => null
+	);
+	
+	public $name;
 	protected $Opauth;
 	protected $strategy;
 	
 	public function __construct(&$Opauth, $strategy){
 		$this->Opauth = $Opauth;
 		$this->strategy = $strategy;
+		
+		$this->name = $strategy['name'];
 	}
 	
 	public function request(){
 	}
 	
 	public function callback(){
+	}
+
+	
+/**
+ * Ensures that a compulsory value is set, throws an error if it's not set
+ * 
+ * @return mixed The loaded value
+ */
+	protected function expects($key, $not = null){
+		if (!array_key_exists($key, $this->strategy)){
+			trigger_error($this->name." config value ($key) expected.", E_USER_ERROR);
+			exit();
+		}
+		
+		$value = $this->strategy[$key];
+		if (empty($value) || $value == $not){
+			trigger_error($this->name." config value ($key) expected.", E_USER_ERROR);
+			exit();
+		}
+		
+		return $value;
+	}
+	
+/**
+ * Loads a default value into $strategy if the associated key is not found
+ * 
+ * @return mixed The loaded value
+ */
+	protected function optional($key, $default = null){
+		if (!array_key_exists($key, $this->strategy)){
+			$this->strategy[$key] = $default;
+			return $default;
+		}
+		
+		else return $this->strategy[$key];
 	}
 }
--- lib/Opauth/Strategy/Facebook.php
+++ lib/Opauth/Strategy/Facebook.php
@@ -1,8 +1,22 @@
 <?php
 class Facebook extends OpauthStrategy{
 	
+/**
+ * Compulsory config keys
+ */
+	public $expects = array('app_id', 'app_secret');
+	
+/**
+ * Optional config keys with respective default values
+ */
+	public $defaults = array(
+		'scope' => null
+	);
+	
 	public function __construct(&$Opauth, $strategy){
 		parent::__construct($Opauth, $strategy);
 		
+		$this->expects('app_id');
+		$this->expects('app_id');
 	}
 }

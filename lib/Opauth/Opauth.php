c2a30199cebb930441f759e95df07cc00a5d4350
U-Zyn Chua <chua@uzyn.com>
2012-03-08 18:29:51 +0800

    Load an arbritary Strategy

--- lib/Opauth/Opauth.php
+++ lib/Opauth/Opauth.php
@@ -53,7 +53,7 @@ class Opauth{
 				require $this->env['LIB'].'OpauthStrategy.php';
 				require $this->env['STRATEGY'].$strategy['name'].'.php';
 				
-				$this->Strategy = new Facebook($this);
+				$this->Strategy = new $strategy['name']($this);
 			}
 			else{
 				trigger_error('Unsupported or undefined Opauth strategy - '.$this->env['strategy'], E_USER_ERROR);

588049eacd22136b588c260868008f0800a6bf24
U-Zyn Chua <chua@uzyn.com>
2012-05-18 20:48:34 +0800

    Handles error auth response

--- example/callback.php
+++ example/callback.php
@@ -56,20 +56,31 @@ switch($Opauth->env['Callback.transport']){
 		echo '<strong style="color: red;">Error: </strong>Unsupported Callback.transport.'."<br>\n";
 		break;
 }
-			
+
 /**
- * Validation
+ * Check if it's an error callback
  */
-if (empty($response['auth']) || empty($response['timestamp']) || empty($response['signature']) || empty($response['auth']['provider']) || empty($response['auth']['uid'])){
-	echo '<strong style="color: red;">Invalid auth response: </strong>Missing key auth response components.'."<br>\n";
+if (array_key_exists('error', $response)){
+	echo '<strong style="color: red;">Authentication error: </strong> Opauth returns error auth response.'."<br>\n";
 }
-elseif (!$Opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason)){
-	echo '<strong style="color: red;">Invalid auth response: </strong>'.$reason.".<br>\n";
-}
+
+/**
+ * No it isn't. Proceed with auth validation
+ */
 else{
-	echo '<strong style="color: green;">OK: </strong>Auth response is validated.'."<br>\n";
-	
-	// Proced with your application-specific authentication
+	if (empty($response['auth']) || empty($response['timestamp']) || empty($response['signature']) || empty($response['auth']['provider']) || empty($response['auth']['uid'])){
+		echo '<strong style="color: red;">Invalid auth response: </strong>Missing key auth response components.'."<br>\n";
+	}
+	elseif (!$Opauth->validate(sha1(print_r($response['auth'], true)), $response['timestamp'], $response['signature'], $reason)){
+		echo '<strong style="color: red;">Invalid auth response: </strong>'.$reason.".<br>\n";
+	}
+	else{
+		echo '<strong style="color: green;">OK: </strong>Auth response is validated.'."<br>\n";
+
+		/**
+		 * It's all good. Go ahead with your application-specific authentication logic
+		 */
+	}
 }
 
 

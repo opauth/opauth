7598c8acc9f60f0ae1d5ef49398b275153069964
U-Zyn Chua <chua@uzyn.com>
2012-05-30 17:12:02 +0800

    Sample auth response

--- tests/Opauth/Strategy/Sample/SampleStrategy.php
+++ tests/Opauth/Strategy/Sample/SampleStrategy.php
@@ -44,4 +44,35 @@ class SampleStrategy extends OpauthStrategy{
 	public function arbritary(){
 		echo 'arbritary() called';
 	}
+	
+	/**
+	 * Successful auth, returns a normal auth response
+	 */
+	public function success(){
+		$this->auth = array(
+			'provider' => 'Sample',
+			'uid' => 1564894354651654,
+			'info' => array(
+				'name' => 'John Doe',
+				'email' => 'john@doe.com',
+				'nickname' => 'john',
+				'first_name' => 'John',
+				'last_name' => 'Doe',
+				'location' => 'Singapore, Singapore',
+				'description' => 'I am the default human',
+				'image' => 'http://example.org/sample.jpg',
+				'phone' => '+65 9000 1000 (ext: 80)',
+				'urls' => array(
+					'website' => 'http://john.doe.com'
+				)
+			),
+			'credentials' => array(
+				'token' => 'g2L]G3^p1yS%83R|N3)Wt;??(L{E%Ff3Y[BfG9gSmw#EM]z~Jl`A~5AMoM({Y{_',
+				'secret' => 'Wl3dwG15nUxmZ0UbRLwyP6IRyeYvvPUtUaxLxI6N07TQPgJ4lIVACQ9Ia1efT89'
+			),
+			'raw' => null
+		);
+		
+		$this->callback();
+	}
 }

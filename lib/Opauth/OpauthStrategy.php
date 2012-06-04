747e58e6a080a59676f04b7f8b238d35a9ab6adc
U-Zyn Chua <chua@uzyn.com>
2012-06-04 18:55:22 +0800

    Explicitly set arg_separator for http_build_query()

--- lib/Opauth/OpauthStrategy.php
+++ lib/Opauth/OpauthStrategy.php
@@ -284,7 +284,7 @@ class OpauthStrategy{
 	 * @param boolean $exit Whether to call exit() right after redirection
 	 */
 	public static function clientGet($url, $data = array(), $exit = true){
-		self::redirect($url.'?'.http_build_query($data), $exit);
+		self::redirect($url.'?'.http_build_query($data, '', '&'), $exit);
 	}
 
 	/**

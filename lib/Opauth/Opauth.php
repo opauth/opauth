d818b53cef51ede10070d9c57d6699cf47583c8a
Ceeram <c33ram@gmail.com>
2013-02-18 10:43:23 +0100

    fixing copyright year

--- lib/Opauth/Opauth.php
+++ lib/Opauth/Opauth.php
@@ -3,7 +3,7 @@
  * Opauth
  * Multi-provider authentication framework for PHP
  *
- * @copyright    Copyright © 2012 U-Zyn Chua (http://uzyn.com)
+ * @copyright    Copyright © 2013 U-Zyn Chua (http://uzyn.com)
  * @link         http://opauth.org
  * @license      MIT License
  */

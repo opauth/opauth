2426880263da1793eff465b9b6d27d62a953e3b7
Dries De Peuter <dries@nousefreak.be>
2013-04-23 17:32:22 +0200

    Use the symfony response to handle the redirect.

--- lib/Opauth/OpauthStrategy.php
+++ lib/Opauth/OpauthStrategy.php
@@ -312,9 +312,15 @@ class OpauthStrategy {
 	 * @param boolean $exit Whether to call exit() right after redirection
 	 */
 	public function redirect($url, $exit = true) {
-		header("Location: $url");
-		if ($exit) {
-			exit();
+		if (isset($this->env['symfony_response'])) {
+			$this->env['symfony_response']->headers->set('Location', $url);
+			// Exit is ignored as the framework should send the headers and save the session.
+		}
+		else {
+			header("Location: $url");
+			if ($exit) {
+				exit();
+			}
 		}
 	}
 	

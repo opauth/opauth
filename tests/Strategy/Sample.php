600eb93683b6a7c07f9a679804c785296d71f6c1
Ceeram <c33ram@gmail.com>
2014-04-04 01:15:01 +0200

    fix typo

--- tests/Strategy/Sample.php
+++ tests/Strategy/Sample.php
@@ -72,7 +72,7 @@ class Sample extends AbstractStrategy
     }
 
     /**
-     * overriding to change visbility to public
+     * overriding to change visibility to public
      */
     public function sessionData($data = null)
     {
@@ -80,7 +80,7 @@ class Sample extends AbstractStrategy
     }
 
     /**
-     * overriding to change visbility to public
+     * overriding to change visibility to public
      */
     public function addParams($configKeys, $params = array())
     {
